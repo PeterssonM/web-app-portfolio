aa88ab5c53d79cb7e3514d9f054fd25e40f54cfb
Maximilian Petersson <maxpet@kth.se>
2025-05-15 14:00:48 +0200

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="maxpet" />
+      </head>
       <body>
         <Navbar />
         {children}
